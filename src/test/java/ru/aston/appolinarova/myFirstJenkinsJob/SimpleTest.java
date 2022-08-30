8b77bac0bbd9f180b2a0c8c7909b2b46478dea32
Anastasia Appolinarova <anastasiia_appolinarova@epam.com>
2022-08-30 16:31:43 +0300

    add test

---
+++ src/test/java/ru/aston/appolinarova/myFirstJenkinsJob/SimpleTest.java
@@ -0,0 +1,13 @@
+package ru.aston.appolinarova.myFirstJenkinsJob;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
