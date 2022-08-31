bb977ca523c7444941fa1dd87a6f68ec1b040a17
Anastasia Appolinarova <anastasiia_appolinarova@epam.com>
2022-08-31 13:58:38 +0300

    change for jenkns comment

--- src/main/java/ru/aston/appolinarova/myFirstJenkinsJob/HelloController.java
+++ src/main/java/ru/aston/appolinarova/myFirstJenkinsJob/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    //commit for test auto install jenkins
 
     @GetMapping("/hello")
     public String sayHello() {
